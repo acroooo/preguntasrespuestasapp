02f1099abe9b602ea5d1223526f1a4c295d7c791
Hernan Chamorro <hernang.work@gmail.com>
2022-02-18 14:35:31 -0300

    '4-install.app.and.Choice.name'

--- prapp/polls/models.py
+++ prapp/polls/models.py
@@ -9,7 +9,7 @@ class Question(models.Model):
         return self.question_text
 
 
-class ChoiceQuestion(models.Model):
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
